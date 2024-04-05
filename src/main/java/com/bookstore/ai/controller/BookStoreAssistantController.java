5a891b9c334a9bb9c0bf328ecc13d7cb0ea6e478
MHPontes <mateushspontes@gmail.com>
2024-04-05 09:19:17 -0300

    Criando bookStoreReview utilizando PromptTemplate da OpenAI.

--- src/main/java/com/bookstore/ai/controller/BookStoreAssistantController.java
+++ src/main/java/com/bookstore/ai/controller/BookStoreAssistantController.java
@@ -1,5 +1,8 @@
 package com.bookstore.ai.controller;
 
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,4 +24,21 @@ public class BookStoreAssistantController {
             defaultValue = "Quais são os livros mais vendidos dos ultimos anos ?") String message) {
         return chatClient.call(message);
     }
+
+//    @GetMapping("/informations")
+//    public ChatResponse bookStoreChat(@RequestParam(value = "message",
+//            defaultValue = "Quais são os livros mais vendidos dos ultimos anos ?") String message) {         //por prompt retornado um ChatResponse do OpenAiChatClient
+//        return chatClient.call(new Prompt(message));
+//    }
+
+    @GetMapping("/reviews")
+    public String bookStoreReview(@RequestParam(value = "book", defaultValue = "Dom Quixote") String book) {
+        PromptTemplate promptTemplate = new PromptTemplate("""
+        Por favor me forneca um breve resumo do livro {book}\s
+        ou informacoes do autor do livro""");        //a ideia e criar um promptTemplate com a pergunta que queremos fazer ao OpenAI ja pronta e direcionar a pergunta do usuario
+        promptTemplate.add("book", book);
+        return this.chatClient.call(promptTemplate.create()).getResult().getOutput().getContent();
+    }
+
+
 }
